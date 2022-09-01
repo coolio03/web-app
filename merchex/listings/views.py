f007fefe24c27687e9ce008757f1ede3edcf7aa3
unknown <coulibalyadama261@yahoo.com>
2022-09-01 10:14:51 +0000

    ntracked files:

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
 
-# Create your views here.
+def hello(request): 
+    bands = Band.objects.all()
+    return render(request, 
+                'listings/hello.html',
+                {'bands': bands})
+
+def about(request):
+    return render(request,
+                'listings/about.html')
+
+def listing(request):
+    listings = Listing.objects.all()
+    return render(request,
+                'listings/listing.html',
+                {'listings': listings})
+
+def contact(request):
+    return render(request,
+                'listings/contact.html')
