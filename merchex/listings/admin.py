63605d5ee2ac90880c6b40e4818ef34870045ca8
unknown <coulibalyadama261@yahoo.com>
2022-09-05 10:32:52 +0000

    CRUD administrateur et Relation 1-n

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from listings.models import Band
+from listings.models import Listing
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+    
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'band')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,43 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Band(models.Model):
+
+    def __str__(self):
+        return f'{self.name}'
+    
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators = [MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_page = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    def __str__(self):
+        return f'{self.title}'
+        
+    class Type(models.TextChoices):
+        disques = "Records"
+        vetements = "Clothings"
+        affiches = "Posters"
+        divers = "Miscellaneous"
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators = [MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    types = models.fields.CharField(choices=Type.choices, max_length=20)
+    band = models.ForeignKey(Band, null= True, on_delete=models.SET_NULL)
